4ae76d64e01a60f455a3af1b8334ba3687492fe2
Tim Bulokhov <timofeybrant@mail.ru>
2025-10-16 21:14:39 +0300

    Initial Commit

---
+++ Loneless/LonelessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LonelessApp.swift
+//  Loneless
+//
+//  Created by Timofey Bulokhov on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct LonelessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
